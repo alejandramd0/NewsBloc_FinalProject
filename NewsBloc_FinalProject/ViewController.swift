c56034756d7abd7d51904644d623be4509b11099
alejandramd0 <alejandradejesus06@gmail.com>
2022-07-19 13:43:01 -0400

    Initial Commit

---
+++ NewsBloc_FinalProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsBloc_FinalProject
+//
+//  Created by Alejandra De Jesus on 7/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
