e9fdf7c70121e5d36539d3e1d98c174bb9a44486
alejandramd0 <alejandradejesus06@gmail.com>
2022-07-21 11:32:21 -0400

    ps. if any of you guys understand constraints and wanna add some to the "the latest" page and knock out some errors that would be fabulous 😅

--- NewsBloc_FinalProject/ViewController.swift
+++ NewsBloc_FinalProject/ViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollPageOne: UIScrollView!
     override func viewDidLoad() {
+       
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
